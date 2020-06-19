6a25583ba12ff187f158d79f07448f80c0d54384
Dave <callremz@gmail.com>
2020-06-19 12:35:59 +0100

    feat: create providers endpoint

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import cors from "cors";
+
 require('dotenv').config();
+const request = require('request-promise');
 
 //create express app
 const app = express();
@@ -14,9 +16,26 @@ app.use(express.urlencoded({ extended: true }));
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
   res.status(200).send('App is running!');
-})
+});
 
-app.post('/sendcredit', (req, res) => {
+app.get("/providers", async (req, res) => {
+  const options = {
+    method: "POST",
+    uri: "https://api.wallets.africa/bills/airtime/providers",
+    json: true
+  }
+  try {
+    const response = await request.post(options).auth(null, null, true, process.env.PUBLIC_KEY);
+    console.log(response);
+    res.status(200).send(response.Providers);
+  } catch (error) {
+    res.status(400).send("An error occured getting providers");
+  }
+  
+  
+});
+
+app.post('/sendairtime', (req, res) => {
   //if not array or empty send back erroe
   if(!req.body.numbers || !Array.isArray(req.body.numbers)){
     res.status(400).json({
@@ -27,7 +46,11 @@ app.post('/sendcredit', (req, res) => {
   //get numbers array from request body
   const numList = req.body.numbers;
 
-  //
+  //iterate through numbers
+  numList.forEach(numRecord => {
+    //send request to paywallet
+    
+  });
 
 });
 
@@ -39,3 +62,4 @@ app.all("*", (req, res) => {
 });
 
 app.listen(port);
+console.log(`App listening on port ${port}`);
